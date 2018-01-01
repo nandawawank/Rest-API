82f290dc2579c1d12f17ebeb4c6c711ab0fa7cfc
nandawawank <nandawawank@gmail.com>
2018-01-01 19:06:42 +0700

    Update controller pull

--- application/controllers/Pull.php
+++ application/controllers/Pull.php
@@ -12,15 +12,43 @@ class Pull extends REST_Controller {
 		$this->load->database();
 	}
 
+	public function event_get(){
+		$id_event = $this->get('id_event');
+		if($id_event == ''){
+			$event = $this->db->get('event')->result();
+		} else{
+			$this->db->where('id_event',$id);
+			$event = $this->db->get('event')->result();
+		}
+	}
+
 	public function index_get(){
-		$id = $this->get('id_dkm');
-		if($id == ''){
+		$id_dkm = $this->get('id_dkm');
+		if($id_dkm == ''){
 			$kontak = $this->db->get('dkm')->result();
 		} else{
 			$this->db->where('id_dkm',$id);
 			$kontak = $this->db->get('dkm')->result();
 		}
 		$this->response($kontak, 200);
+	}
+
+	public function event_post(){
+		$data = array(
+			'id_event'		=> $this->post('id'),
+			'nama_event'	=> $this->post('nama'),
+			'pemateri'		=> $this->post('pemateri'),
+			'lokasi_event'	=> $this->post('lokasi'),
+			'tlp_event'		=> $this->post('telpon'),
+			'waktu_event'	=> $this->post('waktu')
+		);
+		$insert = $this->db->insert('event', $data);
+		if($insert){
+			$this->response($data,200);
+		} else{
+			$this->response(array('status' => 'fail', 502));
+		}
+
 	}
 
 	public function index_post(){
@@ -37,6 +65,27 @@ class Pull extends REST_Controller {
 
 		$insert = $this->db->insert('dkm',$data);
 		if($insert){
+			$this->response($data,200);
+		} else{
+			$this->response(array('status' => 'fail', 502));
+		}
+	}
+
+	public function event_put(){
+		$id_event = $this->put('id');
+		$data = array(
+			'id_event'		=> $this->put('id'),
+			'nama_event'	=> $this->put('nama'),
+			'pemateri'		=> $this->put('pemateri'),
+			'lokasi_event'	=> $this->put('lokasi'),
+			'tlp_event'		=> $this->put('telpon'),
+			'waktu_event'	=> $this->put('waktu')
+		);
+
+		$this->db->where('id_event', $id_event);
+		$update = $this->db->update('event', $data);
+
+		if($update){
 			$this->response($data,200);
 		} else{
 			$this->response(array('status' => 'fail', 502));
